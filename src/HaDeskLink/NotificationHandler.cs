e314dc4b4f85290052de636d0fa6b8ab2db1e5dd
J.A.R.V.I.S. <kibot@kirchweger.de>
2026-05-23 13:20:39 +0200

    fix: add missing using System.Threading in NotificationHandler.cs

--- src/HaDeskLink/NotificationHandler.cs
+++ src/HaDeskLink/NotificationHandler.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace HaDeskLink;
